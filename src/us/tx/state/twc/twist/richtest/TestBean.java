339fd9e3eb4a44edb80a01f0dce956b97d416d83
Keith Athey <keith.athey@twc.state.tx.us>
2010-04-05 14:00:36 -0500

    2
    Signed-off-by: Keith Athey <keith.athey@twc.state.tx.us>

--- src/us/tx/state/twc/twist/richtest/TestBean.java
+++ src/us/tx/state/twc/twist/richtest/TestBean.java
@@ -2,7 +2,7 @@ package us.tx.state.twc.twist.richtest;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
+//comment for scc
 public class TestBean {
 	private Log log = LogFactory.getLog(TestBean.class);
 	
