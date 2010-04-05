1831df3cac1c1d4a2a38c4cac188b1d1ffc91775
Keith Athey <keith.athey@twc.state.tx.us>
2010-04-05 14:13:54 -0500

    fixed it
    Signed-off-by: Keith Athey <keith.athey@twc.state.tx.us>

--- src/us/tx/state/twc/twist/richtest/TestBean.java
+++ src/us/tx/state/twc/twist/richtest/TestBean.java
@@ -2,7 +2,7 @@ package us.tx.state.twc.twist.richtest;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-//comment for scc
+//comment for scc hotfixable
 public class TestBean {
 	private Log log = LogFactory.getLog(TestBean.class);
 	
